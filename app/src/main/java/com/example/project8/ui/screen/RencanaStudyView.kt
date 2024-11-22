00d78c6f829878996184a20ddabb2180b523e147
WisnuDwiSeisno <wseisno37@gmail.com>
2024-11-22 13:22:19 +0700

    menambah variabel listData, dan membuat Column untuk layout dengan background primary

--- app/src/main/java/com/example/project8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/project8/ui/screen/RencanaStudyView.kt
@@ -1,10 +1,19 @@
 package com.example.project8.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.project8.R
 import com.example.project8.model.Mahasiswa
 
 @Composable
@@ -18,4 +27,12 @@ fun RencanaStudyView(
     }
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("")}
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .background(color = colorResource(id = R.color.primary)) ) {
+        Row {
+
+        }
+    }
 }

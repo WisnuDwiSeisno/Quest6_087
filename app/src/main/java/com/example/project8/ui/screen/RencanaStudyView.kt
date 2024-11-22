b8e6b3e873959b60c0f042fe5be6c9d6ad7b65fd
WisnuDwiSeisno <wseisno37@gmail.com>
2024-11-22 15:14:06 +0700

    membuat Column yang akan diisi dengan Text

--- app/src/main/java/com/example/project8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/project8/ui/screen/RencanaStudyView.kt
@@ -1,10 +1,14 @@
 package com.example.project8.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,7 +16,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.project8.R
 import com.example.project8.model.Mahasiswa
@@ -40,7 +46,17 @@ fun RencanaStudyView(
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.umylogo),
+                contentDescription = "",
+                Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+            Spacer(Modifier.padding(start = 16.dp))
+            Column {
 
+            }
         }
     }
 }

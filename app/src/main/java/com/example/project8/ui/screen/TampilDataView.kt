7c4f14a0f9f61a06e75a5adf0da20ecde21f795d
WisnuDwiSeisno <wseisno37@gmail.com>
2024-11-22 16:12:08 +0700

    membuat Column yang akan menampung semua

--- app/src/main/java/com/example/project8/ui/screen/TampilDataView.kt
+++ app/src/main/java/com/example/project8/ui/screen/TampilDataView.kt
@@ -1,2 +1,17 @@
 package com.example.project8.ui.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import com.example.project8.model.Mahasiswa
+import com.example.project8.model.RencanaStudi
+
+@Composable
+fun TampilDataView(
+    mahasiswa: Mahasiswa,
+    rencanaStudi: RencanaStudi,
+    onBackButtonClicked: () -> Unit
+) {
+    Column {
+
+    }
+}

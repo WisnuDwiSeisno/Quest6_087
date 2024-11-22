5aadf56cf44691357c1b305bb6e4110db90f143d
WisnuDwiSeisno <wseisno37@gmail.com>
2024-11-22 14:03:29 +0700

    membuat function MahasiswaApp pada Halaman

---
+++ app/src/main/java/com/example/project8/Halaman.kt
@@ -0,0 +1,15 @@
+package com.example.project8
+
+import androidx.compose.runtime.Composable
+
+enum class Halaman{
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+
+@Composable
+fun MahasiswaApp(){
+
+}

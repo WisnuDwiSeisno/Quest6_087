227acb3cb2f4daca767c7d43a0e6667d34a27189
WisnuDwiSeisno <wseisno37@gmail.com>
2024-11-22 16:17:03 +0700

    membuat Column yang bersisi Nama NIM

--- app/src/main/java/com/example/project8/ui/screen/TampilDataView.kt
+++ app/src/main/java/com/example/project8/ui/screen/TampilDataView.kt
@@ -1,15 +1,20 @@
 package com.example.project8.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.project8.R
 import com.example.project8.model.Mahasiswa
@@ -32,7 +37,15 @@ fun TampilDataView(
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-
+            Image(
+                painter = painterResource(id = R.drawable.umylogo),
+                contentDescription = "",
+                Modifier.size(50.dp)
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Column (Modifier.weight(1f)) {
+                Text(text = mahasiswa.Nama)
+            }
         }
     }
 }
